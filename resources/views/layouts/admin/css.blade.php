61d912ef4cc1a0219dec355d32fc4e128bb0b509
naurarahmafadilah <naura24si@mahasiswa.pcr.ac.id>
2025-10-23 09:42:42 +0700

    Memngubah CSS Pelanggan

---
+++ resources/views/layouts/admin/css.blade.php
@@ -0,0 +1,3 @@
+<link type="text/css" href="{{ asset('assets-admin/css/volt.css') }}" rel="stylesheet">
+<link type="text/css" href="{{ asset('assets-admin/css/volt.css') }}" rel="stylesheet">
+<link type="text/css" href="{{ asset('assets-admin/css/volt.css')}}" rel="stylesheet">
