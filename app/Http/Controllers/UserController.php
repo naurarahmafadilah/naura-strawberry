ef1c196d7cd94114e18beee34345ebc4dd55f391
naurarahmafadilah <naura24si@mahasiswa.pcr.ac.id>
2025-10-23 10:08:20 +0700

    Pertemuan 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $data['dataUser'] = Pelanggan::all();
-        return view('admin.user.index', $data);
+        return view('admin.user.create');
     }
 
     /**
@@ -20,7 +19,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        return view('admin.user.create');
+
     }
 
     /**
@@ -29,18 +28,8 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //dd($request->all());
-
-        $data['first_name'] = $request->first_name;
-        $data['last_name']  = $request->last_name;
-        $data['birthday']   = $request->birthday;
-        $data['gender']     = $request->gender;
-        $data['email']      = $request->email;
-        $data['phone']      = $request->phone;
-
-        Pelanggan::create($data);
-
-        return redirect()->route('user.index')->with('create', 'Penambahan Data Berhasil!');
     }
+
 
     /**
      * Display the specified resource.
@@ -55,8 +44,7 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        $data['dataUser'] = Pelanggan::findOrFail($id);
-        return view('admin.user.edit', $data);
+
     }
 
 /**
@@ -64,18 +52,7 @@ class UserController extends Controller
  */
 public function update(Request $request, string $id)
 {
-    $pelanggan_id = $id;
-    $pelanggan = Pelanggan::findOrFail($pelanggan_id);
 
-    $pelanggan->first_name = $request->first_name;
-    $pelanggan->last_name = $request->last_name;
-    $pelanggan->birthday = $request->birthday;
-    $pelanggan->gender = $request->gender;
-    $pelanggan->email = $request->email;
-    $pelanggan->phone = $request->phone;
-
-    $pelanggan->save();
-    return redirect()->route('user.index')->with('update', 'Perubahan Data Berhasil!');
 }
 
 /**
@@ -83,8 +60,7 @@ public function update(Request $request, string $id)
  */
 public function destroy(string $id)
 {
-    $pelanggan = Pelanggan::findOrFail($id);
 
-    return redirect()->route('user.index')->with('Delete', 'Data berhasil di hapus!');
 }
-};
+
+}
